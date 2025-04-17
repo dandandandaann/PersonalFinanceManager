606ea647167e4d96b19d01d49b64d60c8c7fdb3f
Daniel Dutra <7233639+dandandandaann@users.noreply.github.com>
2025-04-16 22:23:35 -0300

    create deploy powershell script

---
+++ ExpenseLoggerApi/ExpenseLoggerApi/deploy_lambda.ps1
@@ -0,0 +1,3 @@
+﻿cd "C:\repo\BudgetAutomation\ExpenseLoggerApi\ExpenseLoggerApi"
+dotnet lambda deploy-function expense-logger --function-runtime dotnet8 --function-architecture x86_64
+Read-Host -Prompt "Press Enter to exit"
--- ExpenseLoggerApi/SharedLibrary/LogExpenseResponse.cs
+++ ExpenseLoggerApi/SharedLibrary/LogExpenseResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace ExpenseLoggerApi.Model;
 
-public class ResponseModel
+public class LogExpenseResponse
 {
     public bool Success { get; set; }
     public Expense? expense { get; set; }
