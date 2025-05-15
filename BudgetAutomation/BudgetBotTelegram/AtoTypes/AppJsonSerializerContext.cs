5b070814c7022d1d73ec3b9da40d76642080cc7f
Luiz <manuercolle@gmail.com>
2025-05-15 16:08:24 -0300

    fix signup serializable

--- BudgetAutomation/BudgetBotTelegram/AtoTypes/AppJsonSerializerContext.cs
+++ BudgetAutomation/BudgetBotTelegram/AtoTypes/AppJsonSerializerContext.cs
@@ -1,16 +1,19 @@
 ﻿using System.Text.Json.Serialization;
 using Amazon.Lambda.APIGatewayEvents;
 using SharedLibrary;
-using SharedLibrary.UserClasses;
 
 namespace BudgetBotTelegram.AtoTypes;
 
 [JsonSerializable(typeof(LogExpenseResponse))]
-[JsonSerializable(typeof(UserResponse))]
-[JsonSerializable(typeof(UserSignupRequest))]
-[JsonSerializable(typeof(UserExistsResponse))]
+[JsonSerializable(typeof(SharedLibrary.UserClasses.UserResponse))]
+[JsonSerializable(typeof(SharedLibrary.UserClasses.User))]
+[JsonSerializable(typeof(SharedLibrary.UserClasses.UserSignupRequest))]
+[JsonSerializable(typeof(SharedLibrary.UserClasses.UserExistsResponse))]
 [JsonSerializable(typeof(APIGatewayHttpApiV2ProxyRequest))]
 [JsonSerializable(typeof(APIGatewayHttpApiV2ProxyResponse))]
+public partial class AppJsonSerializerContext : JsonSerializerContext
+{
+}
 
 [JsonSerializable(typeof(SharedLibrary.Telegram.Update))]
 [JsonSerializable(typeof(SharedLibrary.Telegram.Chat))]
@@ -18,7 +21,6 @@ namespace BudgetBotTelegram.AtoTypes;
 [JsonSerializable(typeof(SharedLibrary.Telegram.MessageEntity))]
 [JsonSerializable(typeof(SharedLibrary.Telegram.User))]
 [JsonSerializable(typeof(SharedLibrary.Telegram.CallbackQuery))]
-
-public partial class AppJsonSerializerContext : JsonSerializerContext
+public partial class AppTelegramJsonSerializerContext : JsonSerializerContext
 {
 }
