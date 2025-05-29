66872423985904e762066a951a6bf522ba30fecf
Daniel Dutra <7233639+dandandandaann@users.noreply.github.com>
2025-05-29 15:59:39 -0300

    copy telegram classes for ReplyKeybaord

---
+++ BudgetAutomation/SharedLibrary/Telegram/Enums/PollType.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedLibrary.Telegram.Enums;
+
+/// <summary><see cref="Poll"/>: Poll type, currently can be <see cref="Regular">Regular</see> or <see cref="Quiz">Quiz</see></summary>
+public enum PollType
+{
+    /// <summary>“regular” type</summary>
+    Regular = 1,
+    /// <summary>“quiz” type</summary>
+    Quiz,
+}
---
+++ BudgetAutomation/SharedLibrary/Telegram/Types/ReplyMarkups/IKeyboardButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SharedLibrary.Telegram.Types.ReplyMarkups;
+
+/// <summary>Marker interface for a regular or inline button of the reply keyboard</summary>
+[JsonPolymorphic]
+[JsonDerivedType(typeof(InlineKeyboardButton))]
+[JsonDerivedType(typeof(KeyboardButton))]
+public interface IKeyboardButton
+{
+    /// <summary>Text of the button. If none of the optional fields are used, it will be sent as a message when the button is pressed</summary>
+    string Text { get; }
+}
