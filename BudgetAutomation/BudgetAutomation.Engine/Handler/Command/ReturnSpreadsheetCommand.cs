5ada906952996d01a3e11b9cfa0897dd7a2d9ce1
Luiz <manuercolle@gmail.com>
2025-06-17 22:24:21 -0300

    Return Spreadsheet Command implement, add SpreadsheetInfoResponse

---
+++ BudgetAutomation/BudgetAutomation.Engine/Handler/Command/ReturnSpreadsheetCommand.cs
@@ -0,0 +1,87 @@
+﻿using BudgetAutomation.Engine.Enums;
+using BudgetAutomation.Engine.Handler.Command.Alias;
+using BudgetAutomation.Engine.Interface;
+using BudgetAutomation.Engine.Misc;
+using BudgetAutomation.Engine.Model;
+using BudgetAutomation.Engine.Service;
+using SharedLibrary.Enum;
+using SharedLibrary.Telegram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetAutomation.Engine.Handler.Command
+{
+    public class ReturnSpreadsheetCommand(
+        ISenderGateway sender,
+        IUserManagerService userManagerService,
+        ISpreadsheetManagerApiClient spreadsheetManagerApiClient,
+        IChatStateService chatStateService) : ICommand
+    {
+        public string CommandName => StaticCommandName;
+
+        public static string StaticCommandName => "spreadsheetinfo";
+
+        public async Task<Message> HandleAsync(Message message, CancellationToken cancellationToken = default)
+        {
+            UserManagerService.EnsureUserSignedIn();
+
+            if (string.IsNullOrWhiteSpace(UserManagerService.Configuration.SpreadsheetId))
+            {
+                return await sender.ReplyAsync(message.Chat,
+                    $"Por favor configure sua planilha com o commando /{PlanilhaCommandAlias.StaticCommandName} " +
+                    $"antes de usar o comando /{CommandName}.",
+                    cancellationToken: cancellationToken);
+            }
+
+            var chat = message.Chat;
+
+            ArgumentNullException.ThrowIfNull(message.Text);
+
+            var spreadsheetId = UserManagerService.Configuration.SpreadsheetId;
+
+            if (string.IsNullOrWhiteSpace(spreadsheetId))
+            {
+                return await sender.ReplyAsync(chat,
+                    "Você ainda não configurou sua planilha. Use o comando /planilha primeiro.",
+                    "User attempted to undo without configuring a spreadsheet.",
+                    logLevel: LogLevel.Warning,
+                    cancellationToken: cancellationToken);
+            }
+
+            var response =
+                await spreadsheetManagerApiClient.ReturnSpreadsheet(UserManagerService.Configuration.SpreadsheetId, cancellationToken);
+
+            if (!response.Success)
+            {
+                switch (response.ErrorCode)
+                {
+                    case ErrorCodeEnum.ResourceNotFound:
+                        return await sender.ReplyAsync(chat,
+                            "O comando falhou porque a planilha não existem." +
+                            "Tente novamente.",
+                            "User log failed spreadsheet was not found.",
+                            logLevel: LogLevel.Warning,
+                            cancellationToken: cancellationToken);
+                    default:
+                        return await sender.ReplyAsync(chat,
+                            "O comando falhou. Tente novamente.",
+                            "User log failed due to API error.",
+                            logLevel: LogLevel.Warning,
+                            cancellationToken: cancellationToken);
+                }
+            }
+
+            return await sender.ReplyAsync(chat,
+                $"Planilha:{response.SpreadsheetId}",
+                cancellationToken: cancellationToken);
+        }
+
+    public Task<Message> HandleAsync(Message message, ChatState chatState, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
